89ebfba2f30767d00dd4e9ae1493d44dfb800bcf
Bruju <bruyat.ju@live.fr>
2018-11-12 21:56:09 +0100

    Interprétation de types templatés

--- src/fr/bruju/rmconvertisseurfields/Main.java
+++ src/fr/bruju/rmconvertisseurfields/Main.java
@@ -32,10 +32,62 @@ public class Main {
 		table.transformerContenu(Main::transformerSizeField);
 		table.transformerContenu(Main::creerDisposition);
 		table.transformerContenu(Main::retirerDefautRm2k);
+		table.transformerContenu(contenu -> disposer(contenu, "Vector", "Vector"));
+		table.transformerContenu(contenu -> disposer(contenu, "Array", "Array"));
+
+		table.transformerContenu(Main::enleverRef);
+		table.transformerContenu(Main::enleverEnum);
+		table.transformerContenu(contenu -> remplacer(contenu, "ItemAnimation:Ref<Actor>", "Int32"));
 
 
 
 		table.ecrire(CHEMIN_DESTINATION);
+	}
+
+	private static void remplacer(Contenu contenu, String s, String s2) {
+		if (contenu.getDonnees().get(3).equals(s)) {
+			contenu.getDonnees().set(3, s2);
+		}
+	}
+
+	private static void enleverEnum(Contenu contenu) {
+		String nomType = contenu.getDonnees().get(3);
+
+		if (!nomType.startsWith("Enum<") || !nomType.endsWith(">")) {
+			return;
+		}
+
+		contenu.getDonnees().set(3, "Int32");
+	}
+
+	private static void enleverRef(Contenu contenu) {
+		String nomType = contenu.getDonnees().get(3);
+
+		if (!nomType.startsWith("Ref<") || !nomType.endsWith(">")) {
+			return;
+		}
+		nomType = nomType.substring(4, nomType.length() - 1);
+
+		if (nomType.contains(":")) {
+			nomType = nomType.split(":")[1];
+		} else {
+			nomType = "Int32";
+		}
+
+		contenu.getDonnees().set(3, nomType);
+	}
+
+	private static void disposer(Contenu contenu, String typeCpp, String disposition) {
+		String nomType = contenu.getDonnees().get(3);
+
+		if (!nomType.startsWith(typeCpp + "<") || !nomType.endsWith(">")) {
+			return;
+		}
+
+		nomType = nomType.substring(typeCpp.length() + 1, nomType.length() - 1);
+
+		contenu.getDonnees().set(3, nomType);
+		contenu.getDonnees().set(4, disposition);
 	}
 
 	private static void enleverPersistEtis2k3(Contenu contenu) {
