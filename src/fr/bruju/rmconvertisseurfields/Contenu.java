8105aa3356d82461bff5754ad4680d1e6bde33bb
Bruju <bruyat.ju@live.fr>
2018-11-12 20:28:10 +0100

    Quelques operations de transformation simple

--- src/fr/bruju/rmconvertisseurfields/Contenu.java
+++ src/fr/bruju/rmconvertisseurfields/Contenu.java
@@ -4,10 +4,22 @@ import java.util.List;
 import java.util.StringJoiner;
 
 public class Contenu {
+	private static int compteur = 0;
+
 	private final List<String> donnees;
+	private final int id;
+	private final boolean estUnCommentaire;
 
 	public Contenu(List<String> donnees) {
+		if (donnees.get(0).startsWith("#")) {
+			estUnCommentaire = true;
+			donnees.set(0, donnees.get(0).substring(1));
+		} else {
+			estUnCommentaire = false;
+		}
+
 		this.donnees = donnees;
+		id = compteur++;
 	}
 
 	public List<String> getDonnees() {
@@ -15,10 +27,20 @@ public class Contenu {
 	}
 
 	public String serialiser() {
-		StringJoiner sj = new StringJoiner(",");
+		StringJoiner sj;
+		if (estUnCommentaire) {
+			sj = new StringJoiner(",", "#", "");
+		} else {
+			sj = new StringJoiner(",");
+		}
+
 		for (String donnee : donnees) {
 			sj.add(donnee);
 		}
 		return sj.toString();
+	}
+
+	public int getId() {
+		return id;
 	}
 }
--- src/fr/bruju/rmconvertisseurfields/Main.java
+++ src/fr/bruju/rmconvertisseurfields/Main.java
@@ -27,16 +27,56 @@ public class Main {
 			System.exit(1);
 		}
 
+		table.transformerContenu(Main::enleverPersistEtis2k3);
+		table.transformerContenu(Main::ajouterUnChampAuDebut);
+		table.transformerContenu(Main::transformerSizeField);
+		table.transformerContenu(Main::creerDisposition);
+		table.transformerContenu(Main::retirerDefautRm2k);
+
 
 
 		table.ecrire(CHEMIN_DESTINATION);
 	}
 
+	private static void enleverPersistEtis2k3(Contenu contenu) {
+		contenu.getDonnees().remove(6);
+		contenu.getDonnees().remove(6);
+	}
+
+	private static void ajouterUnChampAuDebut(Contenu contenu) {
+		contenu.getDonnees().add(0, Integer.toString(contenu.getId()));
+	}
+
+	private static void transformerSizeField(Contenu contenu) {
+		boolean estSizeField = contenu.getDonnees().remove(3).equals("t");
+
+		if (estSizeField) {
+			contenu.getDonnees().set(3, "SizeField");
+		}
+	}
+
+
+	private static void retirerDefautRm2k(Contenu contenu) {
+		String defaut = contenu.getDonnees().get(6);
+
+		if (defaut.contains("|")) {
+			defaut = defaut.split("\\|")[1];
+			contenu.getDonnees().set(6, defaut);
+		}
+
+	}
+
+	private static void creerDisposition(Contenu contenu) {
+		contenu.getDonnees().add(4, "");
+	}
+
 	private static Contenu creerContenu(String ligne) {
+		/*
 		if (ligne.startsWith("#")) {
 			return null;
 		}
 
+*/
 		String[] champs = ligne.split(",");
 
 		List<String> donnees = new ArrayList<>();
