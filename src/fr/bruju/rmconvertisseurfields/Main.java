dba5c6ebacfa12aec0d0053245d05b6e12b32380
Bruju <bruyat.ju@live.fr>
2018-11-18 18:03:43 +0100

    Debut d'exploitation de la classe Table dans BJUtils

--- src/fr/bruju/rmconvertisseurfields/Main.java
+++ src/fr/bruju/rmconvertisseurfields/Main.java
@@ -1,22 +1,142 @@
 package fr.bruju.rmconvertisseurfields;
 
 import fr.bruju.rmconvertisseurfields.operateur.*;
+import fr.bruju.util.table.Table_;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Stream;
 
 public class Main {
 	public static final String CHEMIN_SOURCE = "A:\\fields.csv";
 	public static final String CHEMIN_DESTINATION = "A:\\javalcfreaderfields.csv";
 
-
 	public static void main(String[] args) {
+		Table_ table = new Table_(new ArrayList<>());
+		remplirTable(table, CHEMIN_SOURCE);
+		appliquerChaineDeTraitements(table);
+		ecrireTable(table, CHEMIN_DESTINATION);
+	}
+
+
+	private static void remplirTable(Table_ table, String cheminSource) {
+		try (Stream<String> flux = Files.lines(Paths.get(cheminSource))) {
+
+			AtomicBoolean booleen = new AtomicBoolean();
+			booleen.set(false);
+
+			flux.forEach(ligne -> {
+				String[] valeurs = ligne.split(",", -1);
+				if (valeurs[0].startsWith("#")) {
+					valeurs[0] = valeurs[0].substring(1);
+				}
+
+				List<String> valeursListees = new ArrayList<>();
+
+				for (String valeur : valeurs) {
+					valeursListees.add(valeur);
+				}
+
+				if (!booleen.getAndSet(true)) {
+					table.ajouterChamps(valeursListees);
+				} else {
+					table.ajouterContenu(valeursListees);
+				}
+			});
+		} catch (IOException e) {
+			System.exit(1);
+		}
+	}
+
+	private static void appliquerChaineDeTraitements(Table_ table) {
+		//table.retirerChamp(table.getPosition("PersistIfDefault"));
+		/*
+		table.retirerChamp("Is2k3");
+		table.ajouterLigne();
+		table.appliquerOperateur(new IntegrationSizeField());
+		table.insererChampApres("Type", "Disposition");
+		table.modifierChamp("Default Value", Main::retirerDefautRm2k);
+
+		table.appliquerOperateur(new Detemplateur("Vector", new DetemplateurDisposeur("Vector")));
+		table.appliquerOperateur(new Detemplateur("Array", new DetemplateurDisposeur("Array")));
+
+		table.appliquerOperateur(new Detemplateur("Enum", d -> { d.type = "Int32"; }));
+		table.appliquerOperateur(new Detemplateur("Ref", new DetemplateurRef()));
+		table.appliquerOperateur(new DeuxPoints());
+		table.appliquerOperateur(new Detemplateur("Ref", new DetemplateurRef()));
+
+
+		table.modifierChamp("Type", chaine -> chaine.equals("ItemAnimation:Ref<Actor>") ? "Int32" : chaine);
+
+		corrigerTable(table);
+		*/
+	}
+
+	private static void ecrireTable(Table_ table, String cheminDestination) {
+		File f = new File(cheminDestination);
+		String s = serialiser(table);
+
+		try {
+			f.createNewFile();
+			FileWriter ff = new FileWriter(f);
+			ff.write(s);
+			ff.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String serialiser(Table_ table) {
+		StringJoiner sb = new StringJoiner("\n", "#", "");
+
+		sb.add(serialiser(table.getColonnes()));
+
+		table.forEach(contenu -> {
+			StringJoiner sj = new StringJoiner(",");
+			contenu.reconstruireObjet((champ, objet) -> {
+				sj.add((String) objet);
+			});
+
+			sb.add(sj.toString());
+		});
+
+		return sb.toString();
+	}
+
+	private static String serialiser(List<String> colonnes) {
+		StringJoiner sj = new StringJoiner(",");
+
+		for (String colonne : colonnes) {
+			sj.add(colonne);
+		}
+
+		return sj.toString();
+	}
+
+
+	// =================================================================================================================
+	// =================================================================================================================
+	// =================================================================================================================
+	// =================================================================================================================
+	// =================================================================================================================
+	// =================================================================================================================
+	// =================================================================================================================
+	// =================================================================================================================
+
+
+
+
+
+
+
+
+
+	public static void main2(String[] args) {
 		Table table = new Table();
 		remplirTable(table, CHEMIN_SOURCE);
 		appliquerChaineDeTraitements(table);
